6de8b279e4a5800a2e8cb7b82a432d05f868bcbf
kazenetu <rakudafactory@gmail.com>
2017-07-07 23:08:37 +0900

    初期コミット

---
+++ ClosedXMLTest/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Load = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 92)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowTemplate.Height = 21
+        Me.DataGridView1.Size = New System.Drawing.Size(547, 272)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Load
+        '
+        Me.Load.Location = New System.Drawing.Point(24, 27)
+        Me.Load.Name = "Load"
+        Me.Load.Size = New System.Drawing.Size(75, 23)
+        Me.Load.TabIndex = 1
+        Me.Load.Text = "読み込み"
+        Me.Load.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(604, 397)
+        Me.Controls.Add(Me.Load)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Load As Button
+End Class
---
+++ ClosedXMLTest/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports ClosedXML.Excel
+
+Public Class Form1
+    Private Sub Load_Click(sender As Object, e As EventArgs) Handles Load.Click
+
+        Dim columnIndexes As New Dictionary(Of String, Integer)
+        Dim rows As New List(Of Dictionary(Of String, String))
+
+        ' xlsx読み込み
+        Using book As New XLWorkbook("dummy.xlsx")
+
+            ' targetシート取得
+            Dim target As IXLWorksheet = book.Worksheet("target")
+
+            ' カラム名の取得
+            For Each column As IXLColumn In target.Columns()
+                Dim columnName As String = target.Cell(1, column.ColumnNumber).GetString()
+                If Not String.IsNullOrEmpty(columnName) Then
+                    columnIndexes.Add(columnName, column.ColumnNumber)
+                End If
+            Next
+
+            ' データ行を取得
+            For Each row As IXLRow In target.Rows(2, target.Rows().LongCount)
+                Dim rowData As New Dictionary(Of String, String)()
+
+                ' セルデータを収集
+                For Each columnName As String In columnIndexes.Keys
+                    Dim value As String = row.Cell(columnIndexes(columnName)).GetString()
+                    rowData(columnName) = value
+                Next
+
+                rows.Add(rowData)
+            Next
+
+        End Using
+
+        ' Datatableに格納
+        Dim result As New DataTable()
+        ' 列の追加
+        For Each columnName As String In columnIndexes.Keys
+            result.Columns.Add(columnName)
+        Next
+
+        ' 行の追加
+        For Each row As Dictionary(Of String, String) In rows
+            Dim newRow As DataRow = result.NewRow
+
+            For Each columnName As String In row.Keys
+                Dim value As String = row(columnName)
+                newRow(columnName) = value
+            Next
+
+            result.Rows.Add(newRow)
+        Next
+
+        ' DataGridViewに設定
+        Me.DataGridView1.DataSource = result
+
+    End Sub
+End Class
